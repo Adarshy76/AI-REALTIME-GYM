010d3a06d197f3f4b7f298fbafa9ce7fbe007e8e
Adarshy76 <adarsh76y@gmail.com>
2026-07-23 09:27:32 +0530

    Fix os.getcwd() paths to be relative to file location (Streamlit Cloud compatibility)

--- main-app/main.py
+++ main-app/main.py
@@ -25,8 +25,9 @@ def main():
         layout="centered"
     )
 
-    load_css(os.path.join(os.getcwd(), "static", "style.css"))
-    inject_local_font(os.path.join(os.getcwd(), "static", "AdobeClean.otf"), "AdobeClean")
+    _app_root = os.path.dirname(os.path.abspath(__file__))
+    load_css(os.path.join(_app_root, "static", "style.css"))
+    inject_local_font(os.path.join(_app_root, "static", "AdobeClean.otf"), "AdobeClean")
 
     init_db()
 
